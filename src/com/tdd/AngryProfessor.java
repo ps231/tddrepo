d0f210fef6df605f5a1883fe79f04aee25a65274
prasad <prasad.shindikar@gmail.com>
2015-06-01 18:16:12 +0530

    final angry prof solution with tests

--- src/com/tdd/AngryProfessor.java
+++ src/com/tdd/AngryProfessor.java
@@ -3,22 +3,22 @@ package com.tdd;
 public class AngryProfessor {
     private final int totalStudents;
     private final int minExpectedStudents;
-    private final String[] studentArrivalTimes;
+    private final int[] studentArrivalTimes;
 
-    public AngryProfessor(final int totalStudents, final int minExpectedStudents, final String[] arrivalTime) {
-        validateArguments(totalStudents, minExpectedStudents, arrivalTime);
-        this.totalStudents = totalStudents;
-        this.minExpectedStudents = minExpectedStudents;
+    public AngryProfessor(final int totStudents, final int minStudents, final int[] arrivalTime) {
+        validateArguments(totStudents, minStudents, arrivalTime);
+        this.totalStudents = totStudents;
+        this.minExpectedStudents = minStudents;
         this.studentArrivalTimes = arrivalTime;
     }
 
-    private void validateArguments(final int totalStudents2, final int minExpectedStudents2, final String[] arrivalTime) {
-        validateMinExpectedStudents(totalStudents2, minExpectedStudents2);
-        validateTotalStudents(totalStudents2);
-        validateStudentArrivalTime(totalStudents2, arrivalTime);
+    private void validateArguments(final int totStudents, final int minStudents, final int[] arrivalTime) {
+        validateMinExpectedStudents(totStudents, minStudents);
+        validateTotalStudents(totStudents);
+        validateStudentArrivalTimes(totStudents, arrivalTime);
     }
 
-    private void validateStudentArrivalTime(final int totalStudents2, final String[] arrivalTime) {
+    private void validateStudentArrivalTimes(final int totalStudents2, final int[] arrivalTime) {
         if (arrivalTime.length != totalStudents2) {
             throwIllegalArgEx("arrival times for all students is not specified");
         }
@@ -46,6 +46,30 @@ public class AngryProfessor {
 
     public int getMinExpectedStudents() {
         return minExpectedStudents;
+    }
+
+    public String isClassCancelled() {
+        int countOfStudentsBeforeTime = 0;
+        for (final int arrivalTime : studentArrivalTimes) {
+            if (getValidatedStudentArrivalTime(arrivalTime) <= 0) {
+                countOfStudentsBeforeTime++;
+            }
+            if (countOfStudentsBeforeTime >= minExpectedStudents) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+
+    private int getValidatedStudentArrivalTime(final int arrivalTime) {
+        if (!arrivalTimeWithinValidRange(arrivalTime)) {
+            throwIllegalArgEx("arrival time must be within valid range");
+        }
+        return arrivalTime;
+    }
+
+    private boolean arrivalTimeWithinValidRange(final int arrivalTime) {
+        return arrivalTime >= -100 && arrivalTime <= 100;
     }
 
 }
--- test/com/tdd/AngryProfessorTest.java
+++ test/com/tdd/AngryProfessorTest.java
@@ -1,5 +1,6 @@
 package com.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AngryProfessorTest {
@@ -26,6 +27,50 @@ public class AngryProfessorTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionIfArrivalTimesOfAllStudentsIsNotSpecified() {
-        new AngryProfessor(2, 1, new String[] { "1" });
+        new AngryProfessor(2, 1, new int[] { 1 });
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfArrivalTimeIsInvalid() {
+        final AngryProfessor ap = new AngryProfessor(2, 2, new int[] { -1, 101 });
+        ap.isClassCancelled();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfArrivalTimeIsInvalid1() {
+        final AngryProfessor ap = new AngryProfessor(2, 2, new int[] { -101, 101 });
+        ap.isClassCancelled();
+    }
+
+    @Test
+    public void shouldReturnNoIfMoreThanExpectedNoOfStudentsHaveEnteredClassBeforeTime() {
+        final AngryProfessor ap = new AngryProfessor(2, 1, new int[] { -1, 0 });
+        Assert.assertEquals("Should have returned true, when no of students entering before time is >= 1", "NO", ap.isClassCancelled());
+    }
+
+    @Test
+    public void shouldReturnNoIfExactlyExpectedNoOfStudentsHaveEnteredClassBeforeTime() {
+        final AngryProfessor ap = new AngryProfessor(2, 1, new int[] { 3, 0 });
+        Assert.assertEquals("Should have returned true, when no of students entering before time is >= 1", "NO", ap.isClassCancelled());
+    }
+
+    @Test
+    public void shouldReturnYesIfLessThanExpectedNoOfStudentsHaveEnteredClassBeforeTime() {
+        final AngryProfessor ap = new AngryProfessor(2, 1, new int[] { 3, 5 });
+        Assert.assertEquals("Should have returned false, when no of students entering before time is < 1", "YES", ap.isClassCancelled());
+    }
+
+    @Test
+    public void shouldReturnYes() {
+        final AngryProfessor ap = new AngryProfessor(2, 2, new int[] { -100, 100 });
+        Assert.assertEquals("Should have returned false, when no of students entering before time is < 1", "YES", ap.isClassCancelled());
+    }
+
+    @Test
+    public void test1() {
+        AngryProfessor ap = new AngryProfessor(4, 3, new int[] { -1, -3, 4, 2 });
+        Assert.assertEquals("Should have returned true, when no of students entering before time is >= 1", "YES", ap.isClassCancelled());
+        ap = new AngryProfessor(4, 2, new int[] { 0, -1, 2, 1 });
+        Assert.assertEquals("Should have returned true, when no of students entering before time is >= 1", "NO", ap.isClassCancelled());
     }
 }

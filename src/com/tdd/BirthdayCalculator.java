e3399b0797d366fbc7d663cf206fb2754bfb4eb2
prasad <prasad.shindikar@gmail.com>
2015-06-02 08:52:14 +0530

    Taum and Diksha Birthday Gift Calculator

---
+++ src/com/tdd/BirthdayCalculator.java
@@ -0,0 +1,103 @@
+package com.tdd;
+
+public class BirthdayCalculator {
+
+    private static final int UPPER_BOUND = 1000000000;
+    private static final int LOWER_BOUND = 0;
+
+    private final long noOfBlackGifts, noOfWhiteGifts, pricePerBlackGift, pricePerWhiteGift, priceOfConversion;
+
+    public BirthdayCalculator(final long blackGifts, final long whiteGifts, final long priceOfBlackGift, final long priceOfWhiteGift, final long convPrice) {
+        validateArguments(blackGifts, whiteGifts, priceOfBlackGift, priceOfWhiteGift, convPrice);
+
+        this.noOfBlackGifts = blackGifts;
+        this.noOfWhiteGifts = whiteGifts;
+        this.pricePerBlackGift = priceOfBlackGift;
+        this.pricePerWhiteGift = priceOfWhiteGift;
+        this.priceOfConversion = convPrice;
+    }
+
+    private void validateArguments(final long blackGifts, final long whiteGifts, final long priceOfBlackGift, final long priceOfWhiteGift, final long convPrice) {
+        validate(blackGifts, whiteGifts, priceOfBlackGift, priceOfWhiteGift, convPrice);
+    }
+
+    private void validate(final long... args) {
+        for (final long arg : args) {
+            if (arg < LOWER_BOUND || arg > UPPER_BOUND) {
+                throw new IllegalArgumentException("argument must be within 0 to 1000000000 range");
+            }
+        }
+
+    }
+
+    public long getNoOfBlackGifts() {
+        return noOfBlackGifts;
+    }
+
+    public long getNoOfWhiteGifts() {
+        return noOfWhiteGifts;
+    }
+
+    public long getPricePerBlackGift() {
+        return pricePerBlackGift;
+    }
+
+    public long getPricePerWhiteGift() {
+        return pricePerWhiteGift;
+    }
+
+    public long getPriceOfConversion() {
+        return priceOfConversion;
+    }
+
+    public long deduceMinAmountToBeSpent() {
+        long minAmount = 0L;
+        if (isBenefitInConversion()) {
+            if (isPriceOfBlackMoreThanPriceOfConversion()) {
+                minAmount += calculateAmount(noOfWhiteGifts, pricePerWhiteGift);
+                minAmount += calculateConvertedPrice(noOfBlackGifts, pricePerWhiteGift, priceOfConversion);
+            }
+            if (isPriceOfWhiteMoreThanPriceOfConversion()) {
+                minAmount += calculateAmount(noOfBlackGifts, pricePerBlackGift);
+                minAmount += calculateConvertedPrice(noOfWhiteGifts, pricePerBlackGift, priceOfConversion);
+            }
+        } else {
+            minAmount += calculateAmount(noOfBlackGifts, pricePerBlackGift);
+            minAmount += calculateAmount(noOfWhiteGifts, pricePerWhiteGift);
+        }
+        return minAmount;
+    }
+
+    private boolean isPriceOfWhiteMoreThanPriceOfConversion() {
+        return (pricePerWhiteGift > pricePerBlackGift + priceOfConversion);
+    }
+
+    private long calculateConvertedPrice(final long noOfGifts, final long pricePerGift, final long priceOfConversion) {
+        return (noOfGifts * pricePerGift + noOfGifts * priceOfConversion);
+    }
+
+    private boolean isPriceOfBlackMoreThanPriceOfConversion() {
+        return (pricePerBlackGift > pricePerWhiteGift + priceOfConversion);
+    }
+
+    private long calculateAmount(final long noOfGifts, final long pricePerGift) {
+        return noOfGifts * pricePerGift;
+    }
+
+    private boolean isBenefitInConversion() {
+        return !(areAllGiftPricesSame() || areConvPricesSameAsSingleGiftPrice() || isConvPriceMoreThanSingleGiftPrice());
+    }
+
+    private boolean isConvPriceMoreThanSingleGiftPrice() {
+        return pricePerBlackGift < priceOfConversion && pricePerWhiteGift < priceOfConversion;
+    }
+
+    private boolean areConvPricesSameAsSingleGiftPrice() {
+        return Math.abs(pricePerBlackGift - pricePerWhiteGift) == priceOfConversion;
+    }
+
+    private boolean areAllGiftPricesSame() {
+        return pricePerBlackGift == pricePerWhiteGift && pricePerWhiteGift == priceOfConversion;
+    }
+
+}
---
+++ test/com/tdd/TaumDikshaBirthDayCalculatorTest.java
@@ -0,0 +1,108 @@
+package com.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaumDikshaBirthDayCalculatorTest {
+
+    private static final int LOWER_THAN_LOWER_BOUND = -1;
+    private static final long HIGHER_THAN_UPPER_BOUND = 10000000001L;
+
+    @Test
+    public void birthdayCalculatorShouldExist() {
+        Assert.assertNotNull(new BirthdayCalculator(10, 10, 1, 1, 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForLowerBoundOnNoOfBlackGifts() {
+        new BirthdayCalculator(LOWER_THAN_LOWER_BOUND, 10, 1, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUpperBoundOnNoOfBlackGifts() {
+        new BirthdayCalculator(HIGHER_THAN_UPPER_BOUND, 10, 1, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForLowerBoundOnNoOfWhiteGifts() {
+        new BirthdayCalculator(10, LOWER_THAN_LOWER_BOUND, 1, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUpperBoundOnNoOfWhiteGifts() {
+        new BirthdayCalculator(10, 10000000001L, 1, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForLowerBoundOnPricePerBlackGifts() {
+        new BirthdayCalculator(10, 10, LOWER_THAN_LOWER_BOUND, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUpperBoundOnPricePerBlackGifts() {
+        new BirthdayCalculator(10, 10, 10000000001L, 1, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForLowerBoundOnPricePerWhiteGifts() {
+        new BirthdayCalculator(10, 10, 1, LOWER_THAN_LOWER_BOUND, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUpperBoundOnPricePerWhiteGifts() {
+        new BirthdayCalculator(10, 10, 1, 10000000001L, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForLowerBoundOnPriceOfConversion() {
+        new BirthdayCalculator(10, 10, 1, 1, LOWER_THAN_LOWER_BOUND);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionForUpperBoundOnPriceOfConversion() {
+        new BirthdayCalculator(10, 10, 1, 1, 10000000001L);
+    }
+
+    @Test
+    public void shouldReturnMinAmountToBeSpentToPurchaseAllBlackAndWhiteGifts() {
+        final BirthdayCalculator bc = new BirthdayCalculator(10, 10, 1, 1, 1);
+        Assert.assertEquals(20, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase2() {
+        final BirthdayCalculator bc = new BirthdayCalculator(10, 10, 3, 1, 1);
+        Assert.assertEquals(30, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase3() {
+        final BirthdayCalculator bc = new BirthdayCalculator(10, 10, 3, 2, 1);
+        Assert.assertEquals(50, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase4() {
+        final BirthdayCalculator bc = new BirthdayCalculator(5, 9, 2, 3, 4);
+        Assert.assertEquals(37, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase5() {
+        final BirthdayCalculator bc = new BirthdayCalculator(3, 6, 9, 1, 1);
+        Assert.assertEquals(12, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase6() {
+        final BirthdayCalculator bc = new BirthdayCalculator(7, 7, 4, 2, 1);
+        Assert.assertEquals(35, bc.deduceMinAmountToBeSpent());
+    }
+
+    @Test
+    public void testCase7() {
+        final BirthdayCalculator bc = new BirthdayCalculator(3, 3, 1, 9, 2);
+        Assert.assertEquals(12, bc.deduceMinAmountToBeSpent());
+    }
+
+}

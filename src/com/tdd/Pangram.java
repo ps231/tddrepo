a1048e932f15a19070ef44e7efc599be29e0e8a9
prasad <prasad.shindikar@gmail.com>
2015-06-03 19:16:50 +0530

    basic pangrams

---
+++ src/com/tdd/Pangram.java
@@ -0,0 +1,34 @@
+package com.tdd;
+
+public class Pangram {
+
+    private String inputStr;
+    private final boolean[] alphabets = new boolean[26];
+
+    public Pangram(final String str) {
+        validateArgument(str);
+        this.inputStr = str.toUpperCase();
+    }
+
+    private void validateArgument(final String str) {
+        if (str == null || str.trim().length() < 1 || str.trim().length() > 1000) {
+            throw new IllegalArgumentException("invalid string argument");
+        }
+    }
+
+    public boolean isPangram() {
+        inputStr = inputStr.replace(" ", "");
+        for (int i = 0; i < inputStr.length(); i++) {
+            final char inputChar = inputStr.charAt(i);
+            final int charIndex = inputChar - 65;
+            alphabets[charIndex] = true;
+        }
+        for (int i = 0; i < alphabets.length; i++) {
+            if (!alphabets[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ test/com/tdd/PangramTest.java
@@ -0,0 +1,45 @@
+package com.tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PangramTest {
+
+    @Test
+    public void pangramMustExist() {
+        Assert.assertNotNull("pangram must exist", new Pangram("a"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfInputStringLengthIsGreaterThanExpected() {
+        new Pangram(new String(new char[1001]));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfInputStringIsNull() {
+        new Pangram(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfInputStringIsEmpty() {
+        new Pangram(" ");
+    }
+
+    @Test
+    public void shouldReturnTrueIfStringIsPangram() {
+        final Pangram p = new Pangram("The quick brown fox jumps over the lazy dog");
+        Assert.assertTrue("should have returned true for pangram string", p.isPangram());
+    }
+
+    @Test
+    public void shouldReturnFalseIfStringIsNotPangram() {
+        final Pangram p = new Pangram("The quick brown fox jumps over the dog");
+        Assert.assertFalse("should have returned false for non pangram string", p.isPangram());
+    }
+
+    @Test
+    public void shouldIgnoreCasesWhileEvaulatingPangram() {
+        final Pangram p = new Pangram("THE quick BROWN fox JUMPS over THE lazy DoG");
+        Assert.assertTrue("should have returned true for pangram string", p.isPangram());
+    }
+}

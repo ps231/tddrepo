8dd1d66ef3068ccdacde9de830ca3fc8f57ee2a5
ps231 <prasad.shindikar@gmail.com>
2016-11-30 13:43:42 -0800

    length of longest substring with non repeating characters

---
+++ src/com/tdd/LengthOfLongestSubstring.java
@@ -0,0 +1,27 @@
+package com.tdd;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+
+    public static  void main(final String... args) {
+        LengthOfLongestSubstring s = new LengthOfLongestSubstring();
+        System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
+    }
+
+    public int lengthOfLongestSubstring(final String s) {
+
+        final Map<Character, Integer> existingCharacters = new HashMap<Character, Integer>();
+        int maxLength = 0;
+
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            if (existingCharacters.containsKey(s.charAt(end))) {
+                start = Math.max(start, existingCharacters.get(s.charAt(end)) + 1);
+            }
+            existingCharacters.put(s.charAt(end), end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}

62df49e9b568d69d7c3bc881b57c77a929e19594
ps231 <prasad.shindikar@gmail.com>
2017-02-16 07:11:41 -0800

    reverse words in a string using extra space

---
+++ src/com/tdd/ReverseWordsInAString.java
@@ -0,0 +1,21 @@
+package com.tdd;
+
+public class ReverseWordsInAString {
+    public static void main(final String... args) {
+        System.out.println("'" + ReverseWordsInAString.reverseWords("how are you") + "'");
+        System.out.println("'" + ReverseWordsInAString.reverseWords("  how are you  ") + "'");
+        System.out.println("'" + ReverseWordsInAString.reverseWords("  how     are   you  ") + "'");
+    }
+
+    public static String reverseWords(String s) {
+        s = s.trim();
+        String[] words = s.split("\\s+");
+        StringBuilder reversedWords = new StringBuilder();
+        for(int i = words.length-1; i>0; i--) {
+            reversedWords.append(words[i].trim());
+            reversedWords.append(" ");
+        }
+        reversedWords.append(words[0].trim());
+        return reversedWords.toString();
+    }
+}

f563ea2334e08071c9366f143c4b02421722d852
ps231 <prasad.shindikar@gmail.com>
2017-08-18 07:48:57 -0700

    inorder successor of a bst, a cleaner solution with better time complexity.

--- src/com/tdd/trees/bst/InorderSuccessorBST.java
+++ src/com/tdd/trees/bst/InorderSuccessorBST.java
@@ -14,45 +14,60 @@ public class InorderSuccessorBST {
     }
 
     private BinaryTreeNode getInorderSuccessor(BinaryTreeNode root, final int value) {
-        final BinaryTreeNode current = findNode(root, value);
-        if (current != null) {
-            if (current.right != null)
-                return findMin(current.right);
-            else {
-                BinaryTreeNode successor = null;
-                BinaryTreeNode ancestor = root;
-                while (ancestor != current) {
-                    if (current.value < ancestor.value) {
-                        successor = ancestor;
-                        ancestor = ancestor.left;
-                    } else {
-                        ancestor = ancestor.right;
-                    }
-                }
-                return successor;
-            }
+        BinaryTreeNode subTree = root;
+        BinaryTreeNode successor = null;
+
+        while (subTree != null) {
+            if (subTree.value > value) {
+                successor = subTree;
+                subTree = subTree.left;
+            } else
+                subTree = subTree.right;
         }
-        return null;
+        return successor;
     }
 
-    private BinaryTreeNode findMin(BinaryTreeNode node) {
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node;
-    }
 
-    private BinaryTreeNode findNode(final BinaryTreeNode root, final int value) {
-        if (root == null)
-            return null;
-
-        if (root.value < value)
-            return findNode(root.right, value);
-        else if (root.value > value)
-            return findNode(root.left, value);
-        else
-            return root;
-    }
+//    private BinaryTreeNode getInorderSuccessor(BinaryTreeNode root, final int value) {
+//        final BinaryTreeNode current = findNode(root, value);
+//        if (current != null) {
+//            if (current.right != null)
+//                return findMin(current.right);
+//            else {
+//                BinaryTreeNode successor = null;
+//                BinaryTreeNode ancestor = root;
+//                while (ancestor != current) {
+//                    if (current.value < ancestor.value) {
+//                        successor = ancestor;
+//                        ancestor = ancestor.left;
+//                    } else {
+//                        ancestor = ancestor.right;
+//                    }
+//                }
+//                return successor;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    private BinaryTreeNode findMin(BinaryTreeNode node) {
+//        while (node.left != null) {
+//            node = node.left;
+//        }
+//        return node;
+//    }
+//
+//    private BinaryTreeNode findNode(final BinaryTreeNode root, final int value) {
+//        if (root == null)
+//            return null;
+//
+//        if (root.value < value)
+//            return findNode(root.right, value);
+//        else if (root.value > value)
+//            return findNode(root.left, value);
+//        else
+//            return root;
+//    }
 
     private static BinaryTreeNode createBinaryTree() {
         final BinaryTreeNode root = new BinaryTreeNode(10, null, null);
